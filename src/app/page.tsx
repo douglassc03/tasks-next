a4a81213c0ef12e4949a3ff222708badc3e3a20f
Douglas <therayjuss@gmail.com>
2024-05-01 20:33:46 -0300

    feat: adding new features to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
-import { Input, Button } from "@nextui-org/react";
-import { AddNewTaskModal } from "@/components/add-new-task-modal";
-import { IoIosSearch } from "react-icons/io";
+import { getTasksOfUser } from "@/actions/get-tasks-of-user"
+import { auth } from "@/app/auth/providers"
+import { TasksTable } from "@/components/tasks-table";
+import { UnfinishedTasks } from "@/components/unfinished-tasks";
+import { Task } from "@prisma/client";
+import { Metadata } from "next";
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'Task Manager - Home',
+  description:
+    "Task Manager is a simple web application to manage your tasks. It allows you to create, edit, delete and mark as done your tasks.",
+};
+
+export default async function Home() {
+  const session = await auth()
+  const tasks = await getTasksOfUser(session?.user.id) as Task[]
+
   return (
-    <div className="flex m-auto justify-center py-12 px-8 max-w-5xl min-h-[calc(100vh-4.125rem)]">
-      <div className="flex gap-8 justify-between w-full">
-        <form className="flex gap-2 w-full">
-          <Input fullWidth className="max-w-xl" />
-          <Button isIconOnly>
-            <IoIosSearch size={18} />
-          </Button>
-        </form>
-        <AddNewTaskModal />
+    <div className="mx-auto flex flex-col p-4 gap-4 max-w-[1300px] overflow-hidden">
+      {tasks.find(task => task.status === 'pending') && <UnfinishedTasks tasks={tasks} />}
+      <div className="bg-zinc-900 flex flex-col rounded-lg p-3 w-full h-full">
+        <TasksTable tasks={tasks} />
       </div>
     </div>
   );
